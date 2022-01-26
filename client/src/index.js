b2cdd6008b6d38dc4a0ff2421ec5f3a9818c9cae
Muthu Palaniyappan OL <muthu892542@gmail.com>
2022-01-26 10:17:38 +0530

    Base Of The Project

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import RootPage from "./RootPage";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RootPage />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
