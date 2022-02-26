c295a72acb9c3eb00536af59db1be3e443567b54
Muthu Palaniyappan OL <muthu892542@gmail.com>
2022-02-26 17:52:07 +0530

    Changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootPage from "./RootPage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<RootPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <RootPage />
   </React.StrictMode>,
   document.getElementById("root")
 );
