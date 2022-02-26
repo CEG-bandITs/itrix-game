4a41c78daeda776f0972d98d4b42e9493f784c38
kumaran_virat <86183777+geekykumaran@users.noreply.github.com>
2022-02-26 20:05:14 +0530

    testing

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import RootPage from "./RootPage";
-
+// testing 
 ReactDOM.render(
   <React.StrictMode>
     <RootPage />
+    
   </React.StrictMode>,
   document.getElementById("root")
 );
