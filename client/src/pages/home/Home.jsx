263db472cd01df19d60fd7a900197f10fa24371a
kumaran_virat <86183777+geekykumaran@users.noreply.github.com>
2022-02-27 10:06:55 +0530

    created image folder in public

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -8,8 +8,11 @@ function Home() {
     <div>
 
       <h2>Home</h2>
-      
+    
       {value.isLogin?"your are  logined":"you are not logined"}
+      <h3>test image</h3>
+      <img src={"/images/test_image.jpg"}></img> 
+
     </div>
   )
 }
