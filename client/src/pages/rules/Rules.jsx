515b149dc42a2bb484b8e5dd4ce9b8722388e52b
Muthu Palaniyappan OL <muthu892542@gmail.com>
2022-04-05 15:27:12 +0530

    rules

--- client/src/pages/rules/Rules.jsx
+++ client/src/pages/rules/Rules.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import "./Rules.css";
+import style from "./Rules.module.css";
+
 function Rules() {
   return (
-    <div className="bg">
-      <section className="one">
-        <div className="section">Rules</div>
-        <div className="ruletext">
-          This is a treasure kind of a game in which the participant should try
-          to find the answer by identifying the similarity and connecting the
-          given images. <br></br>
-          <br></br>
-          The game will be live for three days.<br></br>
-          <br></br>
-          Each day is independent and one winner will be chosen each day.
-          <br></br>
-          <br></br>
-          The UI of the website has no relation to the questions.<br></br>
-          <br></br>
-          Clues will be updated periodically in the specified time.
-        </div>
-      </section>
+    <div className={style.wrapper}>
+      <div className={style.content}>
+        <h1>Rules</h1>
+        <section className={style.ruletext}>
+          <ul>
+            <li>
+              This is a treasure kind of a game in which the participant should
+              try to find the answer by identifying the similarity and
+              connecting the given images.
+            </li>
+            <li>The game will be live for three days.</li>
+            <li>
+              Each day is independent and one winner will be chosen each day.
+            </li>
+            <li>The UI of the website has no relation to the questions.</li>
+            <li>Clues will be updated periodically in the specified time.</li>
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
