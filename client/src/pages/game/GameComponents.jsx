0aeb83dc5de103cbad6f686eed47c999ed50801d
Muthu Palaniyappan OL <muthu892542@gmail.com>
2022-04-28 08:25:09 +0530

    Fixed All Except Hash Checking

--- client/src/pages/game/GameComponents.jsx
+++ client/src/pages/game/GameComponents.jsx
@@ -28,22 +28,32 @@ export function QuestionBar(props) {
   if (props.for === 'Mobile') {
     return (
       <div className={style.QuestionBar}>
-        <img
-          className={style.previcon}
-          src="images/right.png"
-          alt="right image"
-          onClick={IncreaseImgPointer}
-        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-arrow-left-circle-fill"
+          onClick={() => IncreaseImgPointer()}
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+        </svg>
         <img
           className={style.QuestionImg}
           src={data.images[ImgPointer].url}
         ></img>
-        <img
-          className={style.nexticon}
-          src="images/right.png"
-          alt="left image"
-          onClick={DecreaseImgPointer}
-        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-arrow-right-circle-fill"
+          viewBox="0 0 16 16"
+          onClick={() => DecreaseImgPointer()}
+        >
+          <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+        </svg>
       </div>
     )
   } else {
@@ -77,17 +87,18 @@ function HintBox(props) {
           onClick={() => {
             props.setShowHint(false)
           }}
-        ></div>
-        <div className={style.HintBox}>
-          {props.hints.map((i) => {
-            return (
-              <>
-                <p key={i.id}>
-                  Hint {i.id}: &nbsp;&nbsp; {i.msg}
-                </p>
-              </>
-            )
-          })}
+        >
+          <div className={style.HintBox}>
+            {props.hints.map((i) => {
+              return (
+                <>
+                  <p key={i.id}>
+                    Hint {i.id}: &nbsp;&nbsp; {i.msg}
+                  </p>
+                </>
+              )
+            })}
+          </div>
         </div>
       </>
     )
@@ -112,12 +123,11 @@ export function AnswerBar(props) {
   const [disableButton, handleDisableButton] = useState(false)
   const value = React.useContext(Container)
 
-  const FormatAnswer =(message)=>{
-    message = message.trim() ;
-    message = message.split(" ").join("");
-    message =message.toLowerCase() ;
-    return message ;
-
+  const FormatAnswer = (message) => {
+    message = message.trim()
+    message = message.split(' ').join('')
+    message = message.toLowerCase()
+    return message
   }
 
   function Submit(e) {
@@ -125,128 +135,79 @@ export function AnswerBar(props) {
 
     if (answer.trim().length !== 0) {
       handleDisableButton(true)
-      const formattedAnswer = FormatAnswer(answer);
-      SubmitAnswer({ level: value.data.level, answer: formattedAnswer }).then((resp) => {
-        handleDisableButton(false)
+      const formattedAnswer = FormatAnswer(answer)
+      SubmitAnswer({ level: value.data.level, answer: formattedAnswer }).then(
+        (resp) => {
+          handleDisableButton(false)
 
-        if (resp.message === 'Success') {
-          const temp = resp.data
-          if (resp.data == null) {
-            value.handleMessage('Game Over!')
-            return
+          if (resp.message === 'Success') {
+            const temp = resp.data
+            if (resp.data == null) {
+              value.handleMessage('Game Over!')
+              return
+            }
+            const data = {
+              level: temp.level,
+              hints: temp.hints,
+              questions: temp.questions,
+            }
+            value.changeData(data)
+            handleAnswer('')
+            value.handleSuccessModal(true)
+            value.handleErrorMessage('')
+          } else {
+            value.handleErrorMessage('Wrong answer')
+            setTimeout(() => {
+              value.handleErrorMessage('')
+            }, 2500)
           }
-          const data = {
-            level: temp.level,
-            hints: temp.hints,
-            questions: temp.questions,
-          }
-          value.changeData(data)
-          handleAnswer('')
-          value.handleSuccessModal(true);
-          value.handleErrorMessage("");
-        } else {
-          value.handleErrorMessage("Wrong answer");
-          setTimeout(()=>{
-            value.handleErrorMessage("");
-          },2500)
-        }
-      })
+        },
+      )
     } else {
-       value.handleErrorMessage("Answer is Empty") ;
-       setTimeout(()=>{
-        value.handleErrorMessage("");
-      },2500)
+      value.handleErrorMessage('Answer is Empty')
+      setTimeout(() => {
+        value.handleErrorMessage('')
+      }, 2500)
     }
   }
-  if (props.for === 'Mobile') {
-    return (
-      <>
-        <div className={style.AnswerBar}>
-          <form>
-            <input
-              className={style.AnswerBarInput}
-              type="text"
-              value={answer}
-              onChange={(e) => {
-                handleAnswer(e.target.value)
-              }}
-              placeholder="Enter Answer"
-            ></input>
-            <div className={style.AnswerBarBottom}>
-              <button
-                className={style.AnswerBarHint}
-                onClick={(e) => {
-                  e.preventDefault()
-                  setShowHint(true)
-                }}
-              >
-                Hint
-                <img className={style.AnswerBarIcon} src="images/idea.jpg" />
-              </button>
-              <button
-                disabled={disableButton}
-                type="button"
-                className={style.AnswerBarSubmit}
-                onClick={(e) => Submit(e)}
-              >
-                submit
-              </button>
-            </div>
-          </form>
-        </div>
-        <HintBox
-          show={showHint}
-          hints={value.data.hints}
-          setShowHint={setShowHint}
-        />
-      </>
-    )
-  } else {
-    return (
-      <>
-        <div className={style.AnswerBar}>
-          <div className={style.AnswerBarOnelineBottom}>
-            <button
-              className={style.AnswerBarHintlap}
-              onClick={(e) => {
-                e.preventDefault()
-                setShowHint(true)
-              }}
-            >
-              <p>Hint</p>
-              <img className={style.AnswerBarIcon} src="images/idea.jpg" />
-            </button>
-            <input
-              type="text"
-              className={style.AnswerBarInput}
-              placeholder="Enter Answer"
-              value={answer}
-              onChange={(e) => {
-                handleAnswer(e.target.value)
-              }}
-            ></input>
-
-            <button
-              className={style.AnswerBarSubmitlap}
-              type="button"
-              onClick={Submit}
-            >
-              submit
-            </button>
-          </div>
-        </div>
-        <HintBox
-          show={showHint}
-          hints={value.data.hints}
-          setShowHint={setShowHint}
-        />
-      </>
-    )
-  }
+  return (
+    <>
+      <div className={style.AnswerBar}>
+        <form action="">
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              setShowHint(true)
+            }}
+          >
+            Hint
+            <img src="/images/idea_icon.svg" />
+          </button>
+          <input
+            type="text"
+            name="answer"
+            placeholder="Enter Answer"
+            id={answer}
+            onChange={(e) => {
+              handleAnswer(e.target.value)
+            }}
+          />
+          <input
+            type="submit"
+            disabled={disableButton}
+            value="Submit"
+            onClick={(e) => Submit(e)}
+          />
+        </form>
+      </div>
+      <HintBox
+        show={showHint}
+        hints={value.data.hints}
+        setShowHint={setShowHint}
+      />
+    </>
+  )
 }
-
-
-
 
 AnswerBar.propTypes = {
   for: PropTypes.string,
