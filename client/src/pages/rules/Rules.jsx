170f39fcb004b0b98c9ba6f72312a8b554f880d2
Muthu Palaniyappan OL <muthu892542@gmail.com>
2022-04-30 11:26:15 +0530

    Compressed And Fixed Rules Page CSS

--- client/src/pages/rules/Rules.jsx
+++ client/src/pages/rules/Rules.jsx
@@ -7,35 +7,39 @@ import styles from './Rules.module.css'
 function Rules() {
   const size = useWindowSize()
   return (
-     <div className={styles.wrapper}>
-       <div className={styles.menu}>
-
-       <Menu loggedIn={true} desktop={size.width > 1024} />
-       </div> 
-
-       <div className={styles.text}>
-         <div className ={styles.header}>Rules</div>
-         <div className={styles.content}>
-           <div>
-              This is a treasure kind of a game in which the participant should
-              try to find the answer by identifying the similarity and connecting the images.
-          </div>
-          <div>
-              The game will be live for three days.
-          </div>
-          <div>
-             Each day is independent and one winner will be chosen each day.
-          </div>
-          <div>
-             The UI of the website has no relation to the questions.
-          </div>
-          <div>
-             Clues will be updated periodically in the specified time.
+    <main className={styles.main}>
+      <Menu loggedIn={true} desktop={size.width > 1024} />
+      <div className={styles.wrapper}>
+        <div className={styles.text}>
+          <div className={styles.header}>Rules</div>
+          <div className={styles.content}>
+            <ul>
+              <li>
+                This is a treasure kind of a game in which the participant
+                should try to find the answer by identifying the similarity and
+                connecting the images.
+              </li>
+            </ul>
+            <ul>
+              <li>The game will be live for three days. </li>
+            </ul>
+            <ul>
+              <li>
+                Each day is independent and one winner will be chosen each day.
+              </li>
+            </ul>
+            <ul>
+              <li>The UI of the website has no relation to the questions. </li>
+            </ul>
+            <ul>
+              <li>
+                Clues will be updated periodically in the specified time.{' '}
+              </li>
+            </ul>
           </div>
         </div>
-
-        </div>
-    </div>
+      </div>
+    </main>
   )
 }
 
