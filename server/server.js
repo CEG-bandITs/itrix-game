e70d48adc26a98f3aaa7fd96c60d0cbfcf170b45
Muthu Palaniyappan OL <muthu892542@gmail.com>
2023-05-03 17:35:17 +0530

    Finish

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const cookieParser = require('cookie-parser')
 
 const PORT = process.env.PORT || 443
 
-const privateKey = fs.readFileSync('privkey.pem', 'utf8')
-const certificate = fs.readFileSync('./cert.pem', 'utf8')
-const cafile = fs.readFileSync('./chain.pem', 'utf8')
+const privateKey = fs.readFileSync(path.join(__dirname, 'privkey.pem'), 'utf8')
+const certificate = fs.readFileSync(path.join(__dirname, 'cert.pem'), 'utf8')
+const cafile = fs.readFileSync(path.join(__dirname, 'chain.pem'), 'utf8')
 
 const credentials = {
   key: privateKey,
@@ -46,6 +46,10 @@ app.use('/api/game/', require('./routes/GameRoute'))
 
 // leaderboard
 app.use('/api/', require('./routes/LeaderBoardRoute'))
+
+app.get('/.well-known/pki-validation/835E3FBAE9F4105347B0544EE6F197EE.txt', (req, res) => {
+  res.end('E271A851BBFD3AA76D160B7EA2C1080692E1EAA4B01DCFFB9F97C24A71B93248\ncomodoca.com\n7abf155004a165c')
+})
 
 // Serving files from ../client/build
 // Which can be created by running `npm run build` in client folder
